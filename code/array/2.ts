07d140eddd317bbf137cda3b3652a72dc256c3b6
张理健 <zhanglijian@192.168.0.109>
2019-11-26 22:45:56 +0800

    修改例子

--- code/array/2.ts
+++ code/array/2.ts
@@ -1,10 +1,9 @@
 // 914 https://leetcode-cn.com/problems/x-of-a-kind-in-a-deck-of-cards/
 
-import {is} from "@babel/types";
 
 const hasGroupsSizeX = function(deck: Array<number>): boolean {
   // [1,2,3,4,4,3,2,1]
-  if (deck.length === 1 && deck.length > 10000) return false
+  if (deck.length < 1 && deck.length > 10000) return false
   let obj = {}
   let isOver = false
   deck.forEach(card => {
@@ -18,15 +17,33 @@ const hasGroupsSizeX = function(deck: Array<number>): boolean {
   }
   // 是否能分组
   function makeGroup(obj) {
-    let minCount = 0
-    Object.keys(obj).forEach(key => {
-      if (minCount == 0) minCount = obj[key]
+    let minCount = 0, maxCount = 0
+    Object.keys(obj).forEach((key, i) => {
+      if (i === 0) {
+        minCount = obj[key]
+        maxCount = obj[key]
+      }
       if (minCount > obj[key]) minCount = obj[key]
+      if (maxCount < obj[key]) maxCount = obj[key]
     })
+    function findGongYue (min, max) {
+      let result = 0
+      for(let i = 2; i <= min ; i++ ) {
+        if (min % i === 0 && max % i === 0) {
+          result = i
+          break
+        }
+      }
+      return result
+    }
     if (minCount === 1) return false
-    Object.keys(obj).every(key => {
-      return (obj[key] % minCount) === 0
+
+    let num = findGongYue(minCount, maxCount)
+    if (num === 0) return false
+    return Object.keys(obj).every(key => {
+      return (obj[key] % num) === 0
     })
+
   }
   return makeGroup(obj)
 }
