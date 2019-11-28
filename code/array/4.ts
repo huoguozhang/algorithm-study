9f7ae47d6f551860c37400f87621b5da001872f3
zhanglijian <91998142@qq.com>
2019-11-28 17:31:02 +0800

    格雷码

---
+++ code/array/4.ts
@@ -0,0 +1,18 @@
+const grayCode = function(n: number): Array<number> {
+  if (n === 0) return [0]
+  const DigitCount = Math.pow(2, n)
+  const binaryArr = []
+  function fillStr(s, n) {
+    return s.length === n ? s : new Array(n - s.length).fill(0).join('') + s
+  }
+  for (let i = 0 ; i < DigitCount ; i++) {
+    binaryArr.push(fillStr(i.toString(2), n))
+  }
+  return binaryArr.sort((a, b) => {
+    for(let i = 0 ; i < n ; i ++) {
+
+    }
+  })
+}
+
+export default grayCode
