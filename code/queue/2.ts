cab65883b84b1cb3173dc176d295e20b970b8dd4
张理健 <zhanglijian@192.168.0.107>
2019-12-09 23:08:26 +0800

    修改

--- code/queue/2.ts
+++ code/queue/2.ts
@@ -1,32 +1,22 @@
 // 621. 任务调度器 https://leetcode-cn.com/problems/task-scheduler/
 const leastInterval = function(tasks: Array<string>, n: number): number {
-  let result = 0
   if (n === 0) return tasks.length
-  const taskNameMap = {}
-  tasks.forEach(v => {
-    taskNameMap[v] ? taskNameMap[v]++ : taskNameMap[v] = 1
-  })
-  // [A B C D]    n = 4
-  const size = Object.keys(taskNameMap).length // 有多个不同种类的任务
-  // @ts-ignore
-  const maxCountKey = Math.max(...Object.values(taskNameMap))
-  if (maxCountKey === 1) {
-    return result = size
+  const arr = []
+  const len = tasks.length
+  const indexMap = {} // 保存每个key的索引
+  for (let i = 0; i < len; i++) {
+    const v = tasks[i]
+    if (!indexMap.hasOwnProperty(v)) {
+      indexMap[v] = -1
+    }
+    // @ts-ignore
+    const undefinedIndex = arr.findIndex((value, i) => !value && i > indexMap[v] )
+    const index = indexMap[v] === -1 ? undefinedIndex === -1 ? arr.length : undefinedIndex : indexMap[v] + n + 1
+    indexMap[v] = index
+    arr[index] = v
   }
-  // [A A A B B B] n = 1
-  // [A A A B B B] n = 2  8
-  // [A A A B B B] n = 3
-  // [A A A B B B] n = 4
-  const remainCount = tasks.length - maxCountKey // 3
-  let space = (maxCountKey - 1) * n // 2 * n 6
-  // 考虑特殊情况 有n个值和最多值得相等
-  if (size <= n) {
-    // [A A A B C D] n = 3 size = 2
-    result = maxCountKey + (size - 1 ) + space
-  } else {
-    result = maxCountKey + (remainCount <= space ? 0 : remainCount - space) + space
-  }
-  return result
+  console.log(arr)
+  return arr.length
 
 }
 
