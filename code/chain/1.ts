56068b2ddb5d35d51cf9bcc4a02474d8521ed543
zhanglijian <91998142@qq.com>
2019-12-11 16:32:20 +0800

    链表题目

--- code/chain/1.ts
+++ code/chain/1.ts
@@ -1,36 +1,64 @@
 // 增加链表题型  148. 排序链表  https://leetcode-cn.com/problems/sort-list/
+// 声明链表的节点
+
 class Node {
-  constructor (val) {
-    this.val = val
-    this.next = null
+  constructor (value) {
+    this.val = value
+    this.next = undefined
   }
 }
+
+// 声明链表的数据结构
 
 class NodeList {
-
   constructor (arr) {
+    // 声明链表的头部节点
     let head = new Node(arr.shift())
     let next = head
-    while (arr.length) {
-      next.next = new Node(arr.shift())
+    arr.forEach(item => {
+      next.next = new Node(item)
       next = next.next
-    }
+    })
     return head
   }
-
 }
 
-// 输入: 4->2->1->3
-// 输出: 1->2->3->4
+// 交换两个节点的值
+let swap = (p, q) => {
+  let val = p.val
+  p.val = q.val
+  q.val = val
+}
 
-const head = new NodeList([4, 2, 1, 3])
-
-const sortList = function (head) {
-
-  function swqp (p, q) {
-    [p.val, q.val] = [q.val, p.val]
+// 寻找基准元素的节点
+let partion = (begin, end) => {
+  let val = begin.val
+  // 4 5 3 2 1
+  //       p q
+  let p = begin
+  let q = begin.next
+  while (q !== end) {
+    if (q.val < val) {
+      p = p.next
+      swap(p, q)
+    }
+    q = q.next
   }
-
-
-
+  // 让基准元素跑到中间去
+  swap(p, begin)
+  return p
 }
+
+export default function sort (begin, end) {
+  if (begin !== end) {
+    let part = partion(begin, end)
+    sort(begin, part)
+    sort(part.next, end)
+  }
+}
+
+export {
+  Node,
+  NodeList
+}
+
